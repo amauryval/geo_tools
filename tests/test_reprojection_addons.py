0aa3203d1041f07f26fd1a398cbcf9fc2ab2a264
amaury5432 <amauryval@gmail.com>
2019-07-27 00:09:20 +0200

    upd (test_reprojection_addons.py) add new tests and a strange result with pyproj

--- tests/test_reprojection_addons.py
+++ tests/test_reprojection_addons.py
@@ -6,10 +6,21 @@ import pytest
 
 
 def test_ogr_reprojection_same_epsg(point):
-    point_reprojection = core_test().ogr_reprojection(point, 4326, 4326)
-    assert point_reprojection.equals(point)
+    point_reprojected = core_test().ogr_reprojection(point, 4326, 4326)
+    assert point_reprojected.equals(point)
 
 
 def test_pyproj_reprojection_same_epsg(point):
-    point_reprojection = core_test().pyproj_reprojection(point, 4326, 4326)
-    assert point_reprojection.equals(point)
+    point_reprojected = core_test().pyproj_reprojection(point, 4326, 4326)
+    assert point_reprojected.equals(point)
+
+def test_ogr_reprojection_diff_epsg(point):
+    point_reprojected = core_test().ogr_reprojection(point, 4326, 2154)
+    point_reprojected_revert = core_test().ogr_reprojection(point_reprojected, 2154, 4326)
+    assert point_reprojected_revert.equals(point)
+
+def test_pyproj_reprojection_diff_epsg(point):
+    point_reprojected = core_test().pyproj_reprojection(point, 4326, 2154)
+    point_reprojected_revert = core_test().pyproj_reprojection(point_reprojected, 2154, 4326)
+    assert not point_reprojected_revert.equals(point)
+    # Haha unbelievable, pyproj change coordinate precision when reprojecting data ! I don't like it !
