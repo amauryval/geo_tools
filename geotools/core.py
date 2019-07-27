b959db21490188126a2a506544efffa3743b1ba7
amaury5432 <amauryval@gmail.com>
2019-07-27 11:19:08 +0200

    upd (geotools\core.py) add pandas_addons import

--- geotools/core.py
+++ geotools/core.py
@@ -7,11 +7,14 @@ GeoTools Core
 from .helpers.shapely_addons import ShapelyAddons
 from .helpers.reprojection_addons import ReprojectionAddons
 from .helpers.db_addons import DataBaseAddons
+from .helpers.pandas_addons import PandasAddons
+
 
 class GeoTools(
     ShapelyAddons,
     ReprojectionAddons,
-    DataBaseAddons
+    DataBaseAddons,
+    PandasAddons
 ):
 
     __version__ = '0.2'
