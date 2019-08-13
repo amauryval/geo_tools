fbc7488ae9e997197c435663552530be2e021e3e
amaury5432 <amauryval@gmail.com>
2019-08-13 15:52:37 +0200

    clean (general.py)

--- geotools/core/general.py
+++ geotools/core/general.py
@@ -3,8 +3,7 @@ from .logger_addons import LoggerAddons
 
 class GeoToolsCore(LoggerAddons):
 
-    __metaclass__ = ABCMeta
-
+    # __metaclass__ = ABCMeta
     def __init__(self, logger_dir):
         super().__init__(logger_dir=logger_dir)
         self.info_title(self.__class__.__name__)
